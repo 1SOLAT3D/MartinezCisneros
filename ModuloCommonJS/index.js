15f0b70cb266e7d3022c7b5e6cce1a2cf1f758c5
Gianfranco Martínez Cisneros <francodortmund84@gmail.com>
2023-09-01 08:21:45 -0500

    Updated

---
+++ ModuloCommonJS/index.js
@@ -0,0 +1,7 @@
+let frases=[ "Frase 1: Hola", "Frase 2: Como estás? :3", "Frase 3: Adios <3" ]
+
+export function obtieneFrase(indice) {
+    return frases(indice);
+}
+
+module.exports.obtieneFrase=obtieneFrase;
