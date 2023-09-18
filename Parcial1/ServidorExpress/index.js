a0e94351ce9aeff7b86417acbf8335cab40e300c
Gianfranco Martínez Cisneros <francodortmund84@gmail.com>
2023-09-18 07:58:24 -0500

    Updated

--- Parcial1/ServidorExpress/index.js
+++ Parcial1/ServidorExpress/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 app.get("/alumnos", (req, res) => {
-    console.log(req.params);
-    console.log(req.query);
-    console.log(req.body);
-    res.send("Servidor Express contestando a Peticion GET");
+  console.log(req.params);
+  console.log(req.query);
+  console.log(req.body);
+  res.send("Servidor Express contestando a Peticion GET");
 });
 
 app.post("/alumnos", (req, res) => {
-    res.send("Servidor Express contestando a Peticion POST");
- });
+  res.send("Servidor Express contestando a Peticion POST");
+});
 
 app.listen(8080, (req, res) => {
-    console.log("Servidor Express Escuchando");
+  console.log("Servidor Express Escuchando");
 });
