1c1533ca7532b48746e5127e700666a15669cae8
Gianfranco Martínez Cisneros <francodortmund84@gmail.com>
2023-12-04 07:30:33 -0600

    Updated

--- Parcial3/SwaggerTest/__test__/test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../index.js');
-
-describe('Pruebas al método GET de la ruta de usuario', () => {
-  it('Si hago un GET a la ruta /lec2023, debería devolver un status 200', async () => {
-    const response = await request(app).get('/lec2023');
-    expect(response.status).toBe(200);
-  });
-});
